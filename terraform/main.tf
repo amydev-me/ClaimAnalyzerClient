8bbb7a579acc6ac2ee4f7e870528b32054838cd8
Amy <ampyaephyonaing@gmail.com>
2025-07-14 00:10:53 +0800

    Modify terraform inf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+provider "aws" {
+  region = var.region
+}
+
+# Data sources for default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 # CloudWatch Log Group
 resource "aws_cloudwatch_log_group" "app" {
   name              = "/ecs/${var.app_name}-${var.environment}"
@@ -103,4 +119,69 @@ resource "aws_ecs_service" "app" {
   }
 
   depends_on = [aws_lb_listener.app]
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "${var.app_name}-${var.environment}-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS Task Definition
+resource "aws_ecs_task_definition" "app" {
+  family                   = "${var.app_name}-${var.environment}-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode            = "awsvpc"
+  cpu                     = "512"
+  memory                  = "1024"
+  execution_role_arn      = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = var.app_name
+      image = "${var.ecr_repo_uri}:${var.image_tag}"
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.app.name
+          "awslogs-region"        = var.region
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+# Load Balancer Listener
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,16 +14,16 @@ variable "ecr_repo_uri" {
   description = "ECR repository URI"
   type        = string
   default     = "914852670718.dkr.ecr.ap-southeast-1.amazonaws.com/my-react-app"
-} 
-
-variable "environment" {
-    description = "Environment name"
-    type        = string
-    default     = "cicd"
 }
 
 variable "image_tag" {
   description = "Docker image tag"
   type        = string
   default     = "latest"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "cicd"
 }
